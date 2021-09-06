f773dead59d1c4df4fe6d1173e9c860e6778d856
jiyeme <jiyecafe@gmail.com>
2021-09-06 21:04:00 +0800

    fix: 附件下载支持

--- pages/articleView/articleView.js
+++ pages/articleView/articleView.js
@@ -1,4 +1,5 @@
 // pages/articleView/articleView.js
+import { downFilePrepare } from './api'
 const app = getApp()
 var downloadTask = null
 
@@ -9,7 +10,7 @@ Page({
    */
   data: {
     htmlText: "少女祈祷中~",
-    
+
     source: '',
     domain: '',
     path: '',
@@ -37,30 +38,27 @@ Page({
      * 
      */
 
-    console.log('articleView', options)
     this.setData({
       isADClose: app.globalData.isADClose
     })
-    if("undefined" !== typeof options.link)
-    {
+    if ("undefined" !== typeof options.link) {
       this.data.source = 'html'
       this.data.link = options.link;
       this.loadLink(options.link);
       return;
     }
-    
+
     this.data.source = options.source
     this.data.domain = options.domain
-    this.data.path = -1 != options.path.indexOf('?')?encodeURIComponent(options.path):options.path
-    
-    if(0 === this.data.path.indexOf('%2F'))
+    this.data.path = -1 != options.path.indexOf('?') ? encodeURIComponent(options.path) : options.path
+
+    if (0 === this.data.path.indexOf('%2F'))
       this.data.link = 'http://' + this.data.domain + this.data.path;
     else
       this.data.link = 'http://' + this.data.domain + '/' + this.data.path;
     this.loadContent(this.data.path, this.data.source)
   },
-  loadContent: function(data, source)
-  {
+  loadContent: function (data, source) {
     switch (source) {
       case 'html':
         this.loadLink(data);
@@ -88,8 +86,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if("undefined" !== typeof qq && 1 === getCurrentPages().length)
-    {
+    if ("undefined" !== typeof qq && 1 === getCurrentPages().length) {
       this.setData({
         fromShare: true
       })
@@ -126,7 +123,7 @@ Page({
   /**
    * 分享至微信朋友圈
    */
-  onShareTimeline: function(e){
+  onShareTimeline: function (e) {
     // console.log(e)
     return {
       title: this.data.title,
@@ -151,30 +148,27 @@ Page({
    * 加载普通html页面
    * @param {*} link 
    */
-  loadLink: function(url)
-  {
+  loadLink: function (url) {
     wx.request({
       url: url,
       dataType: 'String',
-      success: res=>{
+      success: res => {
         this.setData({
           htmlText: res.data
         })
         var title = res.data.match(/<title>(.*?)<\/title>/i)
-        if(title)
+        if (title)
           this.data.title = title[1]
       },
-      complete: res=>{
+      complete: res => {
         wx.stopPullDownRefresh()
       }
     })
   },
 
   // html链接处理
-  bindLinkHandle: function(e)
-  {
-    if(-1 != e.detail.href.search(/.pdf|.docx|.doc|.xlsx|.xls|.zip|.rar/i))
-    {
+  bindLinkHandle: function (e) {
+    if (-1 != e.detail.href.search(/.pdf|.docx|.doc|.xlsx|.xls|.zip|.rar/i)) {
       // 带有指定后缀，此类链接不进行自动跳转/复制操作
       e.detail.ignore();
 
@@ -182,13 +176,12 @@ Page({
         icon: 'loading',
         title: '正在下载文件',
       })
-      if(0 != e.detail.href.indexOf('http') && 0 != e.detail.href.indexOf('/'))e.detail.href = '/' + e.detail.href;
+      if (0 != e.detail.href.indexOf('http') && 0 != e.detail.href.indexOf('/')) e.detail.href = '/' + e.detail.href;
       let type = e.detail.href.substr(e.detail.href.lastIndexOf('.') + 1);
       wx.downloadFile({
         url: app.globalData.API_DOMAIN + '/File/transferV2/type.' + type + '?link=' + encodeURIComponent(e.detail.href) + '&page=' + this.data.link,
         success: function (res) {
-          if(200 != res.statusCode)
-          {
+          if (200 != res.statusCode) {
             wx.showToast({
               icon: 'none',
               title: parseInt(res.statusCode).toString(),
@@ -212,15 +205,14 @@ Page({
             }
           })
         },
-        fail(err){
+        fail(err) {
           wx.showToast({
-            icon:'none',
+            icon: 'none',
             title: err.error,
           })
         }
       })
-    }else if(-1 != e.detail.href.search(/system\/_content\/download.jsp/i))
-    {
+    } else if (-1 != e.detail.href.search(/system\/_content\/download.jsp/i)) {
       // 此类链接不进行自动跳转/复制
       e.detail.ignore();
 
@@ -228,66 +220,67 @@ Page({
       this.checkDownLink(e.detail.href, '')
     }
   },
-  checkDownLink: function(link, codeValue)
-  {
+  checkDownLink: function (link, codeValue) {
     wx.showLoading({
       title: '正在尝试下载',
     })
-    app.httpPost({
-      url: '/News/downFile',
-      data: {
-        cookie: this.data.cookie,
-        downUrl: link,
-        codeValue: codeValue,
-        domain: this.data.domain
-      },
-    }).then(data=>{
+    downFilePrepare({
+      cookie: this.data.cookie,
+      downUrl: link,
+      codeValue: codeValue,
+      domain: this.data.domain
+    }).then(res => {
+      const resp = res.data;
+      console.log("success", resp)
+      const data = resp.data
       wx.hideLoading()
-      if(!this.data.captchaHidden)this.setData({captchaHidden: true})
-      // 可下载
-      this.downFile(data.link)
-    }).catch(err=>{
-      wx.hideLoading()
-      if(2002 == data.errorCode)
-      {
-        // 需验证码
+      // 需验证码
+      if(data.captcha){
+        console.log('验证码')
         this.setData({
           captchaImg: 'data:image/png;base64,' + data.captcha,
           captchaHidden: false
         })
         this.data.cookie = data.cookie
-      }else{
-        // 其它错误
-        wx.showToast({
-          icon: 'none',
-          title: data.error,
-        })
       }
+      // 可下载
+      else{
+        if (!this.data.captchaHidden) this.setData({ captchaHidden: true })
+          this.downFile(data.link)
+      }
+    }).catch(err => {
+      console.log("error", err)
+      wx.hideLoading()
+      wx.showToast({
+        icon: 'none',
+        title: err.errMsg,
+      })
     })
   },
-  downFile: function(link){
+  downFile: function (link) {
     wx.showLoading({
       title: '正在下载',
     })
     downloadTask = wx.downloadFile({
       url: link,
-      success: (res)=>{
+      success: (res) => {
+        console.log(res)
         wx.showToast({
-          icon:'loading',
+          icon: 'loading',
           title: '尝试打开',
         })
         wx.openDocument({
           filePath: res.tempFilePath,
         })
       },
-      fail: (err)=>{
+      fail: (err) => {
         console.log(err)
       },
-      complete:(res)=>{
-        this.setData({downloadHidden: true})
+      complete: (res) => {
+        this.setData({ downloadHidden: true })
       }
     })
-    downloadTask.onProgressUpdate((p)=>{
+    downloadTask.onProgressUpdate((p) => {
       wx.hideLoading()
       // wx.showLoading({
       //   title: p.progress + "%\r\n" + parseInt(p.totalBytesWritten / 1024) + '/' + parseInt(p.totalBytesExpectedToWrite / 1024) + 'KB',
@@ -295,25 +288,22 @@ Page({
       // console.log("pls stop")
       this.setData({
         downloadHidden: false,
-        progress:p.progress + '%\r\n' + parseInt(p.totalBytesWritten / 1024) + '/' + parseInt(p.totalBytesExpectedToWrite / 1024) + 'KB'
+        progress: p.progress + '%\r\n' + parseInt(p.totalBytesWritten / 1024) + '/' + parseInt(p.totalBytesExpectedToWrite / 1024) + 'KB'
       })
     })
   },
 
   // 提交验证码下载
-  downByYZM: function()
-  {
+  downByYZM: function () {
     this.checkDownLink(this.data.downLink, this.data.codeValue)
   },
 
   // 取消下载
-  downCancel: function()
-  {
-    this.setData({captchaHidden:true})
+  downCancel: function () {
+    this.setData({ captchaHidden: true })
   },
   // 输入验证码
-  bindInputYZM: function(e)
-  {
+  bindInputYZM: function (e) {
     this.data.codeValue = e.detail.value
   },
   // 刷新验证码
@@ -321,8 +311,7 @@ Page({
   },
 
   // 取消下载
-  downloadCancel:function()
-  {
+  downloadCancel: function () {
     this.setData({
       downloadHidden: true,
     })
