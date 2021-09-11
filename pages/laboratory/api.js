e2fa1cc3268ee5ebdc4d940cd284a99ff66cb743
jiyeme <jiyecafe@gmail.com>
2021-09-11 14:03:06 +0800

    fix: 修复实验室

--- pages/laboratory/api.js
+++ pages/laboratory/api.js
@@ -2,10 +2,10 @@ import request from '../../utils/request'
 
 // 获取实验课表首页数据
 export const getLabAll = (param)=>{
-  return request.get("/Jwc/labAll", {param});
+  return request.post("/Jwc/labAll", param);
 }
 
 // 获取实验表详情
 export const getLabDetail = (param)=>{
-  return request.get("/Jwc/labDetail", {param})
+  return request.post("/Jwc/labDetail", param)
 }
